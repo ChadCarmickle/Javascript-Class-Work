975ef25c9cd81d7980525f7067e5833f5b3b1c49
Chadzelda <54717796+Chadzelda@users.noreply.github.com>
2020-06-27 04:05:55 -0400

    Module 1

---
+++ Module 1/Tutorial/Chapter 1 Tutorial/plants.js
@@ -0,0 +1,17 @@
+ /* 
+      JavaScript 6th Edition
+      Chapter 1
+      Chapter case
+
+      Tinley Xeriscapes
+      Plants web page
+      Author: Chad Carmickle 	
+      Date:   3/21/2019
+
+      Filename: plants.js
+   */ 
+   
+				var blanket = "images/blanket.jpg"; 
+				var bluestem = "images/bluestem.jpg"; 
+				var rugosa = "images/rugosa.jpg"; 				
+				
